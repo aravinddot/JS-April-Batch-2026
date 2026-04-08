69a3bb262725df53825b14590358acb0c00d1364
Aravind112703 <aravindmathi93@gmail.com>
2026-04-08 22:00:28 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,145 @@
+// Arithemetic operators
+// Comparison operators
+// Logical operators
+
+
+// i) Arithemetic operators
+
+// addition
+// subraction
+// multiplication
+// division
+// modulus
+// increment
+// decrement
+
+// ii) Comparison operators
+
+// equal to
+// strict equal to 
+// not equal to
+// strict not equal to
+// greater than
+// less than
+// greater than or equal to
+// less than or equal to
+
+// iii) Logical operators
+
+// and
+// or
+// not
+
+
+
+// addition
+
+// let a = 12;
+
+// let b = 8;
+
+// let c = 10;
+
+// console.log(a + b + c);
+
+// subraction
+
+// console.log(a - b)
+
+//multiplication
+
+// console.log(a * b);
+
+// division
+
+// console.log(a / b);
+
+// modulus
+
+// console.log(a % b);
+
+// increment
+// ++
+// console.log(a++); // post increment // 12 a + 1
+// console.log(a); // 13
+// console.log(++b); // pre increment // 9
+
+// decrement
+// --
+// console.log(c--); // post decrement // 10 c - 1
+// console.log(c); // 9
+// console.log(--a); // pre decrement // 12
+
+// exponentation
+
+// console.log(10 ** 10);
+
+
+// let a = 12;
+
+// let b = 8;
+
+// let c = 10;
+
+// equal to
+
+// console.log(12 == "12"); // false
+
+// console.log("new" == "new"); // true
+
+
+// // strict equal to
+
+// console.log(12 === "12"); 
+
+// console.log(12 === 12); 
+
+// not equal to
+
+// console.log(12 != "12"); // false
+
+// strict not equal to
+
+// console.log(12 !== "12"); // true
+
+// let a = 12;
+
+// let b = 8;
+
+// let c = 10;
+
+// // greater than
+
+// console.log(a > b); // true
+
+// // less than
+
+// console.log(a < b); // false
+
+// // greater than or equal to
+
+// console.log(a >= c); // true
+
+// // less than or equal to
+
+// console.log(a <= c); // true
+
+
+// Logical operators
+
+// AND
+
+// console.log(true && true); // true
+
+// console.log(true && false); // false
+
+
+// // OR
+
+// console.log(true || false); // true
+
+// console.log(false || false); // false
+
+// // NOT
+
+// console.log(!true); // false
