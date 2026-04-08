073ef07b63ea8feb5144ed05371e8e6dddf25d55
Aravind112703 <aravindmathi93@gmail.com>
2026-04-08 21:26:13 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,154 @@
+//print the value using console.log()
+
+// console.log("Playwright");
+// console.log("5");
+
+// variables
+
+// var, let, const
+
+// const toolName = "playwright"
+
+// console.log(toolName);
+
+// const toolVersion = 1.20
+
+// console.log(toolVersion);
+
+
+// let value = "playwright"
+
+// value = 'cypress'
+
+// console.log(value);
+
+// var name = "playwright"
+
+// var name = "cypress"
+
+// console.log(name);
+
+
+// single line comment
+
+// <|diff_marker|> PATCH A
+
+// multi line comment
+/*
+    at Object.<anonymous> (/Users/aravindvaishu/PW-April-Batch-2026/variables.js:20:7)
+    at Module._compile (node:internal/modules/cjs/loader:1521:14)
+    at Module._extensions..js (node:internal/modules/cjs/loader:1623:10)
+    at Module.load (node:internal/modules/cjs/loader:1266:32)
+    at Module._load (node:internal/modules/cjs/loader:1091:12)
+    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:164:12)
+    at node:internal/main/run_main_module:28:49
+    any uncaught exception will be thrown here
+    any
+    */
+
+    // camel case
+    // pascal case
+
+    // const toolNameCaseVersion = ""
+
+    // class ToolNameCaseVersion {
+
+    // }
+
+    // const FirstName = "aravind"
+
+    // const LastName = "purushothaman"
+
+
+// Primitive data types
+
+// string, number, boolean, null, undefined
+
+
+// const name = "playwright" // string
+
+// const version = 22.33 // number
+
+// const isOpenSource = false // boolean
+
+// let company = null // null
+
+// let author// undefined
+    
+// console.log(name);
+// console.log(version);
+// console.log(isOpenSource);
+// console.log(company);
+// console.log(author);
+
+
+
+// Non-primitive data types or reference data types
+
+// array, object, function
+
+// array
+
+                
+// const tools = ["playwright", "cypress", "selenium", "nightwatch", "puppeteer", "qtp", "testcomplete"]
+
+// const person = ["aravind", 28, true, null, undefined]
+
+// const nesterArr = ["aravind", 28, true, ["football", "criclket", "selenium"], {name: "aravind", age: 28}]
+
+
+// // console.log(tools[4]);
+
+// // console.log(tools);
+
+
+// console.log(nesterArr[3][0]);
+
+
+
+// object 
+
+
+// const person = {
+//     name: "aravind",
+//     age: 28,
+//     isAlive: true,
+//     hobbies: ["football", "cricket", "selenium"],
+//     name: "purushothaman"
+// }
+
+// // dot notation
+
+// console.log(person.hobbies[0]);
+
+// // bracket notation
+
+// console.log(person["name"]);
+
+
+
+// function
+
+// function print() {
+
+//     console.log("playwright is powerfull automation tool");
+
+// } 
+
+// print()
+
+// print()
+
+// print()
+
+// print()
+
+// print()
+
+
+
+
+
+
+
+
