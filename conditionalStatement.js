5520401506583a56f4ff1a134b9c877c05355f1b
Aravind112703 <aravindmathi93@gmail.com>
2026-04-09 11:57:48 +0530

    conditionalStatement

---
+++ conditionalStatement.js
@@ -0,0 +1,103 @@
+
+
+// if - statement
+
+// const personOneAge = 28
+
+// const personTwoAge = 30
+
+// if(personOneAge == personTwoAge) { 
+//     console.log("person 1 name = playwright");
+//     console.log("person 2 name = cypress");   
+// }
+
+
+
+// if - else statement
+
+
+// if() {
+
+// } else {
+
+// }
+
+// if(personOneAge == personTwoAge) {
+//     console.log("person 1 name = playwright");
+//     console.log("person 2 name = cypress");   
+// } else {
+//     console.log("person 1 name = selenium");
+//     console.log("person 2 name = nightwatch"); 
+// }
+
+
+// if - else if - else statement
+
+
+// if()  {
+
+// } else if() {
+
+// } else if () {
+
+// } else {
+
+// }
+
+// const percentage = 100
+
+// if(percentage >= 90) {
+//     console.log("grade A");
+// } else if(percentage >= 80) {
+//     console.log("grade B");
+// } else if(percentage >= 70) {
+//     console.log("grade C");
+// } else if(percentage >= 60) {
+//     console.log("grade D");
+// } else {
+//     console.log("grade F");
+// }
+
+
+// maths = 75
+
+
+// switch statement
+
+
+// switch() {
+//     case: 
+
+//     break;
+
+//     case: 
+
+//     break
+
+//     case: 
+
+//     break;
+
+//     default:
+// }
+
+const day = 1;
+
+switch(day) {
+    case 1:
+        console.log("monday");
+    break;
+
+    case 2:
+        console.log("tuesday");
+    break;
+
+    case 3:
+        console.log("wednesday");
+    break;
+
+    default:
+        console.log("invalid day");
+}
+
+
